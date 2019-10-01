e1d593af307921b8be30a50fe17ad77c0306955d
Jorge Ossa <jorgehossa@gmail.com>
2019-10-01 17:54:31 -0500

    Start Configure Firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          This is my first App whit React!
-        </h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.error(`Error ${error.code}: ${error.message}`))
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            This is my first App whit React!
+          </h1>
+          <button onClick={this.handleAuth}>Google Login</button>
+        </header>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const API_KEY_FIREBASE = process.env.REACT_APP_FIREBASE_API_KEY;
+firebase.initializeApp({
+    apiKey: `${API_KEY_FIREBASE}`,
+    authDomain: "instaphoto-5920c.firebaseapp.com",
+    databaseURL: "https://instaphoto-5920c.firebaseio.com",
+    projectId: "instaphoto-5920c",
+    storageBucket: "",
+    messagingSenderId: "360404220671",
+    appId: "1:360404220671:web:d87c0e9c05b15d822f2aca",
+    measurementId: "G-SLJFG6VJCN",
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
